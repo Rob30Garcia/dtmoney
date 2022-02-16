730fe88d6888b2bc996e352c8e906cd85efaf3ac
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-02-16 18:01:10 -0400

    style: styled cards

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,5 +1,36 @@
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import { Container } from "./styles";
+
+
 export function Summary() {
   return (
-    <h1>Summary</h1>
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={income} alt="entradas" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={outcome} alt="saidas" />
+        </header>
+        <strong>R$ 1.259,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={total} alt="total" />
+        </header>
+        <strong>R$ 16.141,00</strong>
+      </div>
+    </Container>
   );
 }
