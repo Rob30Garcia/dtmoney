52c82203a92a6987249ccaf9db78163cd2cf2cb3
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-02-18 17:13:54 -0400

    style: styled forms

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 Modal.setAppElement('#root');
 
@@ -12,6 +13,31 @@ export function NewTransactionModal({ isOpen, onRequestClose}: ModalProps) {
     <Modal 
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-    />
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    >
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input
+          placeholder='Título'
+        />
+
+        <input 
+          type='number'
+          placeholder='Valor'
+        />
+
+        <input 
+          placeholder='Categoria'
+        />
+
+        <button
+          type="submit"
+        >
+          Cadastrar
+        </button>
+      </Container>
+    </Modal>
   );
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,50 @@
 import styled from 'styled-components';
 
 export const Container = styled.form`
-  
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  input {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    border-radius: 0.25rem;
+
+    border: 1px solid var(--input-background);
+    background: var(--input-background);
+
+    font-size: 1rem;
+    font-weight: 400;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+    width: 100%;
+    padding: 0 1.25rem;
+    height: 4rem;
+    background: var(--green);
+    color: #FFF;
+    font-size: 1rem;
+    font-weight: 600;
+
+    border: 0;
+    border-radius: 0.25rem;
+    margin-top: 1.5rem;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
 `;
