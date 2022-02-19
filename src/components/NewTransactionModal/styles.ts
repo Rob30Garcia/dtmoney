c67eebae7e6ff9cdb8e5ecb992ed20750ea0ea4e
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-02-19 11:10:14 -0400

    chore: active function to choice transaction type in button
    - type state
    - colors when set type

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
+
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const color = {
+  red: '#E52E4D',
+  green: '#33CC95'
+}
 
 export const Container = styled.form`
   h2 {
@@ -56,23 +66,6 @@ export const TransactionTypeContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem; //space-between
 
-  button {
-    height: 4rem;
-    border: 1px solid var(--form-background);
-    border-radius: 0.25rem;
-
-    background: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    transition: border-color 0.2s;
-
-    &:hover {
-      border-color: ${darken(0.1, '#d7d7d7')};
-    }
-  }
-
   img {
     width: 20px;
     height: 20px;
@@ -83,5 +76,25 @@ export const TransactionTypeContainer = styled.div`
     margin-left: 1rem;
     font-size: 1rem;
     color: var(--text-title);
+  }
+`;
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid var(--form-background);
+  border-radius: 0.25rem;
+
+  background: ${(props) => 
+    props.isActive ? 
+      transparentize(0.9, color[props.activeColor]) : 
+      'transparent'};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')};
   }
 `;
